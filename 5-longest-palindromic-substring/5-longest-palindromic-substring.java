976e269feeae11cc75c6b66abf36d46095c539f9
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-08 16:25:12 +0530

    Time: 26 ms (95.13%), Space: 43.2 MB (68.89%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,38 +1,38 @@
 class Solution {
     public String longestPalindrome(String s) {
-     int n=s.length();
-        int low;
-        int high;
-        char arr[]=s.toCharArray();
-        int start=0;
-        int end=1;
-        for(int i=1;i<n;i++){
-            // FOR ODDSUBSTRING
-            low=i-1;
-             high=i+1;
-            while(low>=0 && high<n && arr[low]==arr[high]){
-                if(high-low+1>end){
-                start=low;
-                end=high-low+1;
+       int n=s.length();
+       char []arr=s.toCharArray();
+       int start=0;
+        int end=1;// intially size will be one;
+       for(int i=1;i<n;i++){  // SINCE I HAVE TO TAKE TWO POINTER
+           // IF EVEN STRING
+           int low=i-1;
+           int high=i;
+           while(low>=0 && high<n && arr[low]==arr[high]){
+               // if current length is greater
+               if(high-low+1>end){
+                 start=low;
+                 end=high-low+1;
                }
-                low--;
-                high++;
-            }
-            // FOR EVENSUBSTRING
-            low=i-1;
-             high=i;
-            while(low>=0 && high<n && arr[low]==arr[high]){
-                if(high-low+1>end){
-                start=low;
-                end=high-low+1;
+               low--;
+               high++;
+           }
+           
+           // IF ODD STRING
+            low=i;// of
+            high=i;
+           while(low>=0 && high<n && arr[low]==arr[high]){
+                // if current length is greater
+               if(high-low+1>end){
+                 start=low;
+                 end=high-low+1;
                }
-                low--;
-                high++;
-            }
-            
-            
-        }
-        return s.substring(start,start+end);
+               low--;
+               high++;
+           }
+          
+       }
+        return s.substring(start,start+end); 
     }
     
 }
