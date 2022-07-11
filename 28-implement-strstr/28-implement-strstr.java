9cee3a7f88aa34a736de1f533c0026b33dd42ef7
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-11 15:57:45 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (62.59%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+     return haystack.indexOf(needle);   
+    }
+    public int rabin(String txt, String pat){
+        return 0;
+    }
+}
