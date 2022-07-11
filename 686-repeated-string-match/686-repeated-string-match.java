fbd982864a7514f403d01eed01452d64e62a0e18
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-11 15:43:21 +0530

    Time: 7 ms (97.60%), Space: 42.7 MB (74.35%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,13 +1,60 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
       // BRUTE FORCE
-        for(int i=0; i<a.length(); i++){
-            int j;
-            for(j=0; j<b.length() && a.charAt((i+j)%a.length())==b.charAt(j);j++);
-            if(j==b.length())return (j+i-1)/a.length()+1;
+        int count=1;
+        StringBuilder text=new StringBuilder(a);
+        while(text.length()<b.length()){
+           count++;
+            text.append(a);
         }
+        if(findPat(text.toString(),b))return count;
+        text.append(a);
+        if(findPat(text.toString(),b))return count+1;
+        
         return -1;
         
+    }
+    public boolean findPat(String a, String b){
+        int n=a.length();
+        int m=b.length();
+        int maxChar=256,prime=101,highestPow=1;
+        for(int i=0;i<m-1;i++){
+            highestPow=(highestPow*maxChar)%prime; 
+        }
+        int textHash=0;
+        int patHash=0;
         
+//----------------------------------------------------------------------------------
+        
+        for(int i=0;i<m;i++){       // Calculated hashfor both text and pattern
+            textHash=(textHash*maxChar+a.charAt(i))%prime;
+            patHash=(patHash*maxChar+b.charAt(i))%prime;
+        }
+        
+//----------------------------------------------------------------------------------        
+        for(int i=0; i<n-m+1; i++){
+            
+            if(patHash==textHash){
+                boolean check=true;
+                for(int j=0; j<m; j++){
+                    if(a.charAt(i+j)!=b.charAt(j)){
+                        check=false;
+                        break;
+                    }
+                }
+                if(check){
+                    return true;
+                }
+            }
+            if(i<n-m){
+                textHash = (textHash-highestPow*a.charAt(i));// Substracting first temp of window
+                textHash=(textHash*maxChar)%prime; // increasing power for next window;
+                textHash=(textHash+a.charAt(i+m))%prime;// adding next window element
+                if(textHash<0)textHash+=prime;
+            }
+            
+        }
+        
+        return false;
     }
 }
