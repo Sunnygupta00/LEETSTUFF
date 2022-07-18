87a8a9178b6495f6566ea0d07a0812a41d1531cd
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-18 23:12:35 +0530

    Time: 2 ms (12.21%), Space: 42.5 MB (31.63%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -1,11 +1,24 @@
 
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root==null)return true;
-        return same(root.left, root.right);         
-    }
-    public boolean same(TreeNode left,TreeNode right){
-        if(left==null || right==null)return left==right;
-        return left.val==right.val && same(left.left,right.right) && same( left.right,right.left);
+      if(root==null)return true;
+        Queue<TreeNode> q =new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+        while(q.size()>0){
+           int size = q.size();
+            for(int i=0;i<size/2;i++){
+                TreeNode left =q.poll();
+                TreeNode right= q.poll();
+                if(left==null && right==null)continue;
+                if(left==null || right ==null)return false;
+                if(left.val != right.val)return false;
+                q.offer(left.left);          // QUESTION IS WHY WE ARE ADDING NULL VALUES, BECASE VALUES CAN BE ALWAYS SAME, BUT ALIGNMENT NOT 
+                q.offer(right.right);
+                q.offer(left.right);
+                q.offer(right.left);
+            }
+        }
+        return true;
     }
 }
