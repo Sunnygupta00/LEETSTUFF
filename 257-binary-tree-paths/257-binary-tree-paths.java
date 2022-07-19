cbeb2189cfd7a39b791e78c2a50e8499a4e95d0d
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-19 23:33:04 +0530

    Time: 14 ms (33.37%), Space: 43.2 MB (59.57%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+       List<String> ans =new ArrayList<>();
+        rec(root,"",ans);
+        return ans;
+    }
+    public void rec(TreeNode root, String temp, List<String> ans){
+        if(root.right==null && root.left == null){
+            ans.add(temp+root.val);
+            return;
+        }if(root.left!=null) rec(root.left, temp+root.val+"->", ans);
+          if(root.right!=null) rec(root.right, temp+root.val+"->", ans);
+    }
+}
