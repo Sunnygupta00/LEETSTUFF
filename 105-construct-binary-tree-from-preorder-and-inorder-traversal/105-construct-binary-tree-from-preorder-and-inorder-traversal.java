b552f7b5a8df9dfffd39113d9053eebd0dae7492
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-23 23:34:13 +0530

    Time: 6 ms (26.24%), Space: 44.8 MB (28.97%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,24 +1,20 @@
 class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length != inorder.length)return null;
-        int length=preorder.length-1;
-        return build(preorder, 0, length, inorder, 0, length);
+    public TreeNode buildTree(int[] pre, int[] in) {
+        if(pre.length != in.length)return null;
+        int length = in.length-1;
+        return build(in, 0, length, pre, 0, length);
     }
-    public TreeNode build( int pre[], int preStart, int preEnd, int in[], int inStart, int inEnd){
-        if(inStart>inEnd){
-            return null;
-        }
+    public TreeNode build(int in[], int inStart, int inEnd, int pre[], int preStart, int preEnd){
+        if(inStart>inEnd)return null;
         TreeNode root = new TreeNode (pre[preStart]);
         int index = inStart;
-        for(; index<= inEnd; index++){
-            if(root.val == in[index]){
-                break;
-            }
+        for(;index<= inEnd; index++){
+            if(root.val == in[index])break;
         }
         int leftSize = index - inStart;
         int rightSize = inEnd - index;
-        root.left = build( pre, preStart+1, preStart+1+leftSize-1, in, inStart, index-1);
-        root.right = build( pre, preStart+leftSize+1, preEnd, in, index+1, inEnd);
-            return root;
+        root.left = build(in, inStart, index-1, pre, preStart+1, preStart+leftSize);
+        root.right = build(in ,index+1, inEnd, pre, preStart+leftSize+1, preEnd);
+        return root;
     }
 }
