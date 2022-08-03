8cebd2a7a322825bd01be058090b436cca0baffb
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-08-03 10:20:35 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (5.42%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -1,28 +1,28 @@
 
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-       List<Integer>ans=new ArrayList<>();   
-       TreeNode curr = root;
+       List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
         while(curr!=null){
-            if(curr.left==null){
+            if(curr.left == null){
                 ans.add(curr.val);
-                curr = curr.right;
+                curr = curr.right; // BHAI LEFT NULL HAI RIGHT SIDE DEKH lo
             }else{
-                TreeNode prev = curr.left;
-                while(prev.right != null && prev.right != curr){
+                TreeNode prev = curr.left; // left ke baad root pr ane ka jugad
+                while(prev.right !=null && prev.right != curr){
                     prev = prev.right;
                 }
-                if(prev.right == null){ // make connection
+                if(prev.right == null) { // malab hamne connection nahi banaya
                     prev.right = curr;
-                    curr = curr.left; // go to left and make another connection
-                    
-                }else{
+                    curr = curr.left ;// now go for next left 
+                }else{      // means mera connection bana hua hai, means i have visited left side, so break the connection and go right
                     prev.right = null;
                     ans.add(curr.val);
                     curr = curr.right;
                 }
+                
             }
         }
-     return ans;   
+        return ans;
     }
 }
