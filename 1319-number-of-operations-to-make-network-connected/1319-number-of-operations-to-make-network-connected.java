853120ed769047d8a3a5eb7854c7092f095e706b
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-08-09 21:59:33 +0530

    Time: 27 ms (46.94%), Space: 72 MB (43.76%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,35 +1,31 @@
 class Solution {
-    public int makeConnected(int n, int[][] com) {
-        if(com.length < n-1){
-            return -1;
+    public int makeConnected(int n, int[][] connections) {
+     if(connections.length < n-1)return -1;   // MINIMUM EDGE
+        List<Integer> adj[] = new List[n];
+        for(int i=0; i<n; i++){
+            adj[i] = new ArrayList<>();
         }
-      List<Integer> adj[] = new List [n];
-        for(int i=0;i<n;i++){
-            adj[i]= new LinkedList<>();
-        }
-        for(int c[] : com){
+        boolean vis[] = new boolean[n];
+        for(int []c : connections){
             adj[c[0]].add(c[1]);
             adj[c[1]].add(c[0]);
         }
-        boolean vis[] = new boolean[n];
-        int count =0;
-        for(int i = 0; i<n; i++ ){
-            if(vis[i]== false){
-                dfs(adj, vis, i);
-                count++;
-            }
-        }
+        int count = 0;
+       for(int i =0; i<n; i++){
+           if(vis[i] == false){
+               dfs(adj, vis, i);
+              count++;
+           }
+       }
         return count-1;
-        
     }
-    public void dfs(List<Integer> adj[], boolean vis[], int source){
-        if(vis[source]== true){
-            return;
+    public void dfs(List<Integer>adj[], boolean []vis, int source){
+        if(vis[source] == true){
+            return ;
         }
-        // add to ans
-        vis[source]= true;
-        for(int i : adj[source]){
-            dfs(adj, vis, i);
+        vis[source] = true;
+        for( int u : adj[source]){
+            dfs(adj, vis, u);
         }
     }
 }
