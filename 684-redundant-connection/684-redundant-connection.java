0cc68fef49e33dbef7e1ba13450e34bc6967f2ff
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-08-09 23:40:45 +0530

    Time: 18 ms (11.64%), Space: 47.9 MB (15.48%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.java
@@ -0,0 +1,43 @@
+class Solution {
+    private int a=0,b=0;
+    public int[] findRedundantConnection(int[][] edges) {
+        if(edges.length == 0)return new int[]{0,0};
+        // LETS CREATE GRAPH
+        int n = edges.length+1;
+        List<List<Integer>> adj = new ArrayList<>(n);
+        for(int i = 0; i<n; i++ ){
+            adj.add(new ArrayList<>());
+        }
+        boolean vis[] = new boolean[n];
+        int ans []= new int[2];
+        for(int c[]: edges){
+            Arrays.fill(vis, false);
+            adj.get(c[0]).add(c[1]);
+            adj.get(c[1]).add(c[0]);
+            if(dfs(adj, vis, c[0], -1)){
+                return c;
+            }
+        }
+        return ans;
+    }
+    public boolean dfs(List<List<Integer>> adj, boolean vis[],int source, int parent){
+        vis[source] = true;
+        for(int i: adj.get(source)){
+            if(vis[i] == false){
+                if(dfs(adj, vis, i, source)){
+                    return true;
+                }
+            }else{
+                if(parent != i){
+                    a = parent;
+                    b= i;
+                    return true;
+                }
+            }
+        }
+        
+     return false;
+    }
+    
+    
+}
