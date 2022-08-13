eac3b8d75611472bea006f76833bfd82311122f0
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-08-14 01:41:08 +0530

    Time: 1 ms (89.95%), Space: 53.9 MB (43.20%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -14,19 +14,17 @@ class Solution {
         
     }
     public boolean bfs(int[][] graph, int node, int color[]){
-         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(node,0));
+         Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
         color[node]=0;
         while(q.size()>0){
-             Pair curr = q.poll();
-            int index = curr.i;
-            int col = curr.color;
+            int index = q.poll();
             //System.out.println(index+" "+col);
             for(int  i: graph[index]){
                // System.out.println(i);
                 if(color[i]==-1){
-                    color[i]= col == 0 ? 1 : 0;
-                    q.offer(new Pair(i,color[i]));
+                    color[i]= color[index] == 0 ? 1 : 0;
+                    q.offer(i);
                 }else{
                     if(color[i] == color[index]){
                         return false;
@@ -35,13 +33,5 @@ class Solution {
             }
         }
         return true;
-    }
-}
-class Pair{
-    int i;
-    int color;
-    Pair(int x,int y){
-        i=x;
-        color=y;
     }
 }
