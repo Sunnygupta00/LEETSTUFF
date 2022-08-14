36f2bd4281e3eb83650ffbf3f4236937983a4a13
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-08-15 01:42:58 +0530

    Time: 11 ms (48.26%), Space: 49.4 MB (52.14%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int toposort[] = new int[numCourses];
+        List<List<Integer>>adj = new ArrayList<>();
+        int n= prerequisites.length;
+         int indegree[] = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int c[] : prerequisites){
+            adj.get(c[1]).add(c[0]);
+            
+        }
+       // CALCULATING INDEGREE
+        for(int i =0;i<numCourses;i++){
+            for(int j: adj.get(i)){
+                indegree[j]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        int count =0;
+        while(q.size()>0){
+            int curr = q.poll();
+            toposort[count] = curr;    
+            count++;
+            for(int i: adj.get(curr)){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.offer(i);
+                }
+            }
+        }
+        return count==numCourses ? toposort : new int[0];
+        
+    
+    }
+}
