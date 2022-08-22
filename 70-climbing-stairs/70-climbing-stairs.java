3cf60803f2e94632923c1d65ebd86eccaf71d926
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-08-22 22:43:52 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (60.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,14 @@
-//MEMOIZATION
+//TABULATION
 class Solution {
     public int climbStairs(int n) {
+        if(n ==1 || n==2)return n;
           int dp[] = new int [n+1];
-        Arrays.fill(dp, -1);
-        return rec(n,dp);
-    }
-    public int rec(int n, int dp[]){
-         if(n ==1 || n==2){
-            return n;
+          Arrays.fill(dp, -1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]; // sum of last two stair combinaton
         }
-        if(dp[n]!=-1)return dp[n];
-        return dp[n] = rec(n-1, dp)+rec(n-2, dp);
+        return dp[n];
     }
 }
