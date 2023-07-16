1a2cf3420b827fea0b48b6f36828fbc437eac331
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-07-16 15:26:43 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -33,30 +33,29 @@ class GFG
 
 //User function Template for Java
 
+
 class Solution{
 
 
     static Boolean isSubsetSum(int n, int arr[], int sum){
-        // code here
-        int dp[][] = new int[n][sum+1];
+        return subsetSumToK(n, sum, arr);
+    }
+     public static boolean subsetSumToK(int n, int k, int arr[]){
+        int dp[][] = new int[n][k+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i], -1);
         }
-        return rec(n-1, arr, sum,dp)>0;
-    }
-    static int rec(int n, int arr[], int sum, int dp[][]){
-        if(n<0 || sum <=0){
-            if(sum == 0){
+        return rec(n-1, k, arr, dp)!=0;
+       }
+        public static int rec(int n, int k, int arr[], int dp[][]){
+        if(n<0 || k<=0 ){
+            if(k == 0){
                 return 1;
             }else{
                 return 0;
             }
         }
-        if(dp[n][sum]!=-1){
-            return dp[n][sum];
-        }
-         int pick = rec(n-1, arr, sum-arr[n], dp);
-          int notPick = rec(n-1, arr, sum, dp);
-          return dp[n][sum]=Math.max(pick, notPick);
+        if(dp[n][k] != -1)return dp[n][k];
+        return dp[n][k]=rec(n-1, k, arr, dp)+ rec(n-1, k-arr[n], arr,dp);
     }
 }
